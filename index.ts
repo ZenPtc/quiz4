6da7a1fc5577b7de5f388eeff1e0d277d6a2e206
TK_PTC <50984892+ZenPtc@users.noreply.github.com>
2021-03-09 16:17:13 +0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,184 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { body, query, validationResult } from 'express-validator'
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 3000
+const SECRET = "SIMPLE_SECRET"
+
+interface JWTPayload {
+  username: string;
+  password: string;
+}
+
+interface User{
+  username:string,
+  password:string,
+  firstname:string,
+  lastname:string,
+  balance:number
+}
+let users:User[] = [];
+
+app.post('/login',
+  (req, res) => {
+
+    const { username, password } = req.body
+    // Use username and password to create token.
+    const token = jwt.sign({username,password},SECRET)
+    const AEuser = users.find(user => user.username === username)
+    if(!AEuser){
+      return res.status(400).json({
+        message: 'Invalid username or password'
+      })
+    }
+    if(AEuser.password !== password){
+      return res.status(400).json({
+        message: 'Invalid username or password'
+      })
+    }
+    return res.status(200).json({
+      message: 'Login succesfully',
+      token: token
+    })
+  })
+
+app.post('/register',
+  (req, res) => {
+
+    const { username, password, firstname, lastname, balance } = req.body
+    const user = {username, password, firstname, lastname, balance}
+    //already exist
+    const AEuser = users.find(user => user.username === username)
+    if(AEuser){
+      res.status(400)
+      res.json({
+      message: "Username is already in used"
+    })
+    return
+    }
+    //pass
+    users.push(user)
+    res.status(200)
+    res.json({
+      message: "Register successfully"
+    })
+    return
+  })
+
+app.get('/balance',
+  (req, res) => {
+    const token = req.query.token as string
+    try {
+      const { username } = jwt.verify(token, SECRET) as JWTPayload
+      const AEuser = users.find(user => user.username === username)
+      res.status(200)
+      res.json({
+        "name": AEuser?.username,
+        "balance": AEuser?.balance
+      })
+    }
+    catch (e) {
+      //response in case of invalid token
+      res.status(401)
+      res.json({
+          message: "Invalid token"
+      })
+      return
+    }
+  })
+
+app.post('/deposit',
+  body('amount').isInt({ min: 1 }),
+  (req, res) => {
+    const { amount } = req.body
+    const token = req.query?.token as string
+    try {
+      const { username } = jwt.verify(token, SECRET) as JWTPayload
+      const AEuser = users.find(user => user.username === username)
+      //Is amount <= 0 ?
+      if (!validationResult(req).isEmpty())
+        return res.status(400).json({ message: "Invalid data" })
+      //can deposit
+      if(!AEuser){
+        return res.status(400).json({ message: "Invalid data" })
+      }else{AEuser.balance = AEuser.balance + amount}
+      res.status(200)
+      res.json({
+        message: "Deposit successfully",
+        "balance": AEuser?.balance
+      })
+    }
+    catch (e) {
+      //response in case of invalid token
+      res.status(401)
+      res.json({
+          message: "Invalid token"
+      })
+      return
+    }
+  })
+
+app.post('/withdraw',
+  body('amount').isInt({ min: 1 }),
+  (req, res) => {
+    const { amount } = req.body
+    const token = req.query?.token as string
+    try {
+      const { username } = jwt.verify(token, SECRET) as JWTPayload
+      const AEuser = users.find(user => user.username === username)
+      //Is amount <= 0 ?
+      if (!validationResult(req).isEmpty())
+        return res.status(400).json({ message: "Invalid data" })
+      //can deposit
+      if(!AEuser){
+        return res.status(400).json({ message: "Invalid data" })
+      }else{AEuser.balance = AEuser.balance - amount}
+      res.status(200)
+      res.json({
+        message: "Withdraw successfully",
+        "balance": AEuser?.balance
+      })
+    }
+    catch (e) {
+      //response in case of invalid token
+      res.status(401)
+      res.json({
+          message: "Invalid token"
+      })
+      return
+    }
+  })
+
+app.delete('/reset', (req, res) => {
+
+  //code your database reset here
+  users = []
+  return res.status(200).json({
+    message: 'Reset database successfully'
+  })
+})
+
+app.get('/me', (req, res) => {
+  res.status(200)
+  res.json({
+    firstname: "Naruporn",
+    lastname: "Powthongchin",
+    code: 620610794,
+    gpa: 3.38
+  })
+})
+
+app.get('/demo', (req, res) => {
+  return res.status(200).json({
+    message: 'This message is returned from demo route.'
+  })
+})
+
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
